630a7af815b5478bbedc5ed6e5fc16351d0d6193
Hernandes Junio de Assis <hernandesjunio@gmail.com>
2021-02-05 20:09:17 -0300

    feat: implementação

---
+++ README.md
@@ -0,0 +1,155 @@
+<div align="right">
+  
+  [![Portuguese](https://www.countryflags.io/br/flat/32.png)](README.md)
+    
+</div>
+
+<p align="center">
+  <img alt="Conversão de Inteiro por extenso" src=".github/logo.png" width="250px"/>
+</p>
+
+<p align="center">
+  <img alt="Top Language" src="https://img.shields.io/github/languages/top/hernandesjunio/inteiro-por-extenso?color=3498db&style=for-the-badge">
+  <img alt="Repository Size" src="https://img.shields.io/github/repo-size/hernandesjunio/inteiro-por-extenso?color=3498db&style=for-the-badge">
+  <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/hernandesjunio/inteiro-por-extenso?color=3498db&style=for-the-badge">   
+  <a href="https://github.com/hernandesjunio">
+    <img alt="Developed by Hernandes Junio" src="https://img.shields.io/badge/Developer-Hernandes-%3498db?color=3498db&style=for-the-badge">
+  </a>  
+  <img alt="Project License" src="https://img.shields.io/apm/l/vim-mode?style=for-the-badge"/>  
+</p>
+
+<p align="center">
+ <a href="#eye_speech_bubble-visualizar">Visualizar</a> •
+ <a href="#information_source-sobre">Sobre</a> •
+ <a href="#arrow_forward-executar">Executar</a> •
+ <a href="#hammer_and_wrench-tecnologias">Tecnologias</a> • 
+ <a href="#sparkles-funcionalidades">Funcionalidades</a> •
+ <a href="#boy-autor">Autor</a> •
+ <a href="#balance_scale-licença">Licença</a>
+</p>
+
+---
+
+## :eye_speech_bubble: **Visualizar**
+
+<div align="center">
+
+| Pré Visualização  
+| :------------------------------------------------------------------------------------:
+| <kbd><img src=".github/preview.gif" alt="Desktop"/></kbd>
+
+</div>
+  
+---
+
+## :information_source: Sobre
+
+<div align="center">
+
+Algoritmo para conversão de números positivos inteiros para extenso.
+
+---
+
+</div>
+
+## :arrow_forward: **Executar**
+
+<div align="center">
+
+Para executar esse projeto você precisa baixar este repositório, ter o Gerenciador de Pacotes do Node ([`NPM`](https://www.npmjs.com/get-npm)) ou o Gerenciador de Pacotes YARN ([`YARN`](https://yarnpkg.com/getting-started)) instalado.
+
+Abra o prompt de comando no diretório do projeto, e execute os seguintes códigos:
+
+<details>
+  <summary><i>com <b>npm</b></i></summary>
+  
+```bash
+# Instalar dependências
+$ npm install ou npm i
+
+# Executar os testes com Jest
+
+$ npm test
+
+````
+
+</details>
+
+<details>
+<summary><i>com <b>yarn</b></i></summary>
+
+```bash
+# Instalar dependências
+$ yarn install
+
+# Executar os testes com Jest
+$ npm test
+
+````
+
+</details>
+
+> ⚠️ Será gerado arquivo HTML (test-report.html) com o resultado dos testes que pode ser aberto no navegador.
+
+</div>
+
+---
+
+## :hammer_and_wrench: **Tecnologias**
+
+<div align="center">
+
+|                                      👨‍💻                                      |
+| :--------------------------------------------------------------------------: |
+|             [TypeScript 3.9.7](https://www.typescriptlang.org/)              |
+|              [Jest](https://jestjs.io/docs/en/getting-started)               |
+|               [Ts-Jest](https://www.npmjs.com/package/ts-jest)               |
+|    [Jest-html-reporter](https://www.npmjs.com/package/jest-html-reporter)    |
+|                        [Node](https://nodejs.org/en/)                        |
+|                 [Husky](https://www.npmjs.com/package/husky)                 |
+| [Git-Commit-Msg-Linter](https://www.npmjs.com/package/git-commit-msg-linter) |
+
+</div>
+
+---
+
+## :sparkles: **Funcionalidades**
+
+<div align="center">
+
+|              :page_facing_up:              |
+| :----------------------------------------: |
+| Conversão de números inteiros para extenso |
+
+</div>
+
+---
+
+## 🧑 **Autor**
+
+<div align="center">
+
+<a href="https://github.com/hernandesjunio">
+ <img src="https://avatars1.githubusercontent.com/u/9919?s=200&v=4" width="100px;" alt="Profile Photo Hernandes Junio"/>
+ <br/>
+ <sub><b>Hernandes Junio de Assis</b></sub>
+</a>
+
+Desenvolvido com por Hernandes 👋🏽 Meus Contatos!
+
+[![Linkedin Badge](https://img.shields.io/badge/-Hernandes-blue?style=flat-square&logo=Linkedin&logoColor=white)](https://www.linkedin.com/in/hernandesjunio/)
+[![Github Badge](https://img.shields.io/badge/-Hernandes-000?style=flat-square&logo=Github&logoColor=white)](https://github.com/hernandesjunio)
+[![Outlook Badge](https://img.shields.io/badge/-Hernandes-0078d4?style=flat-square&logo=microsoft-outlook&logoColor=white)](mailto:hernandesjunio@gmail.com)
+
+</div>
+
+---
+
+## :balance_scale: **Licença**
+
+<div align="center">
+
+Copyright © 2021 [Hernandes Junio](https://github.com/hernandesjunio).<br />
+This project is licensed by [MIT](./LICENSE).
+
+</div>
---
+++ src/main.ts
@@ -0,0 +1,186 @@
+function capitalize(text: string) {
+  return text && [text[0].toUpperCase(), ...text.slice(1)].join('')
+}
+
+function truncate(numero: number, length: number) {
+  const ini = numero.toString().length - length;
+  const fim = ini + length;
+  return +numero.toString().substring(ini, fim)
+}
+
+function ignorarDigitos(numero: number, quantidade: number) {
+  const referencia = Math.pow(10, quantidade);
+  return Math.floor(numero / referencia) * referencia;
+}
+
+function invalid(): string {
+  throw new RangeError('Number out of range for conversion.');
+}
+
+function ateZero(numero: number) {
+  return numero < 0 && invalid() || numero === 0 && 'Zero' || ''
+}
+
+function de1a9(numero: number) {
+  var dic = {
+    1: 'um',
+    2: 'dois',
+    3: 'três',
+    4: 'quatro',
+    5: 'cinco',
+    6: 'seis',
+    7: 'sete',
+    8: 'oito',
+    9: 'nove'
+  };
+
+  const zeroNumber = numero <= 0 && ateZero(numero)||'';
+  var prefix = numero > 20 && ' e ' || '';
+  numero = truncate(numero, 1);
+  return zeroNumber.concat((numero > 0 && prefix || '').concat(capitalize(dic[numero]) || ''));
+}
+
+function de10a19(numero: number) {
+  var dic = {
+    10: 'dez',
+    11: 'onze',
+    12: 'doze',
+    13: 'treze',
+    14: 'quatorze',
+    15: 'quinze',
+    16: 'dezesseis',
+    17: 'dezessete',
+    18: 'dezoito',
+    19: 'dezenove'
+  };
+
+  var prefix = numero > 20 && ' e ' || '';
+  numero = truncate(numero, 2);
+  return numero > 9 && numero < 20 && prefix.concat(capitalize(dic[numero])) || '';
+}
+
+function de20a90(numero: number) {
+  var dic = {
+    20: 'vinte',
+    30: 'trinta',
+    40: 'quarenta',
+    50: 'cinquenta',
+    60: 'sessenta',
+    70: 'setenta',
+    80: 'oitenta',
+    90: 'noventa',
+  };
+
+  var prefix = numero > 99 && ' e ' || '';
+  const sufix2 = de1a9(numero);
+  numero = ignorarDigitos(truncate(numero, 2), 1)
+  const prefix2 = capitalize(dic[numero]) || ''
+  return (numero > 19 && numero < 99 && prefix || '').concat(prefix2).concat(sufix2)
+}
+
+function de1a99(numero: number) {
+  return truncate(numero, 2) < 20 && de10a19(numero) || de20a90(numero);
+}
+
+function de100a199(numero: number) {
+  var dic = {
+    0: 'm',
+    1: 'nto',
+    100: 'ce'
+  }
+  const pluralIndex = +(numero % 100 !== 0)
+  const sufix2 = de1a99(numero)
+  numero = ignorarDigitos(truncate(numero, 3), 2);
+  const prefix = numero > 99 && capitalize(dic[numero]) || '';
+  const sufix1 = prefix && numero > 99 && dic[pluralIndex] || '';
+
+  return prefix.concat(sufix1).concat(sufix2);
+}
+
+function de200a999(numero: number) {
+  var dic = {
+    200: 'duzentos',
+    300: 'trezentos',
+    400: 'quatrocentos',
+    500: 'quinhentos',
+    600: 'seiscentos',
+    700: 'setecentos',
+    800: 'oitocentos',
+    900: 'novecentos',
+  };
+
+  const sufix = de1a99(numero);  
+  numero = ignorarDigitos(truncate(numero, 3), 2);
+  const prefix = capitalize(dic[numero] || '');
+
+  return prefix.concat(sufix)
+}
+
+function de1a999(numero: number) {
+  return truncate(numero, 3) < 200 && de100a199(numero) || de200a999(numero);
+}
+
+function de1000a1999(numero: number) {
+  var dic = {
+    1000: 'mil',
+  };
+  const sufix = de1a999(numero);
+  numero = ignorarDigitos(truncate(numero, 4), 3);
+  const prefix = numero > 999 && numero < 2000 && capitalize(dic[numero].concat(' ')) || '';
+  return prefix.concat(sufix && sufix.trim() || '');
+}
+
+function de2000a999999(numero: number) {
+  const sufix = de1a999(numero);
+  numero = ignorarDigitos(truncate(numero, 6), 3);
+  const fator = 1000;
+  const milhar = numero / fator;
+  const prefix = milhar && de1a999(milhar).concat(' Mil') || '';
+  return prefix.concat(sufix && ' ' || '').concat(sufix && sufix.trim() || '')
+}
+
+function de1a999999(numero: number) {
+  return truncate(numero, 6) < 2000 && de1000a1999(numero) || de2000a999999(numero)
+}
+
+function de1ma999999m(numero: number) {
+  var dic = {
+    0: 'ão',
+    1: 'ões',
+    1000000: 'milh',
+  };
+
+  const milhao = 1000000;
+  const sufix2 = de1a999999(numero)
+  numero = ignorarDigitos(truncate(numero, 9), 6);
+  const prefix1 = numero >= milhao && de1a999(Math.floor(numero / milhao)).concat(' ') || ''
+  const prefix2 = numero >= milhao && capitalize(dic[milhao]) || '';
+  const prefix3 = numero && de1a999999(numero).concat(capitalize(dic[milhao])) || ''
+  const sufix1 = prefix3 && (Math.floor(numero / milhao) == 1 && dic[0] || dic[1]).concat(' ') || ''
+
+  return prefix1.concat(prefix2).concat(sufix1).concat(sufix2);
+}
+
+function de1ba999999b(numero: number) {
+  var dic = {
+    0: 'ão',
+    1: 'ões',
+    1000000000: 'bilh'
+  };
+
+  const bilhao = 1000000000;
+  const sufix2 = de1ma999999m(numero)
+  const bilhoes = Math.floor(numero / bilhao)
+  const prefix1 = bilhoes > 0 && de1a999(bilhoes).concat(' ') || ''
+  numero = ignorarDigitos(truncate(numero, 12), 9);
+  const prefix2 = numero >= bilhao && capitalize(dic[bilhao]) || '';
+  const prefix3 = numero && de1a999999(numero).concat(capitalize(dic[bilhao])) || ''
+  const sufix1 = prefix3 && (numero / bilhao == 1 && dic[0] || dic[1]).concat(' ') || ''
+  return prefix1.concat(prefix2).concat(sufix1).concat(sufix2);
+}
+
+export function integerToWords(numero: number) {
+  return de1ba999999b(numero).trim();
+}
+
+console.log(integerToWords(1297));
